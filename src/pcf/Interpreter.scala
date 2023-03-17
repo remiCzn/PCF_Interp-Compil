fc8bfba3d18e4732c4a22e3b8634423c13bd5992
remiCzn <cazinremi2@gmail.com>
2023-03-17 22:17:37 +0100

    erreur de type

--- src/pcf/Interpreter.scala
+++ src/pcf/Interpreter.scala
@@ -1,7 +1,6 @@
 package pcf
 
 import ast.{AST, Term, transform}
-import gen.Gen
 import interp.{IceCube, Value}
 import org.antlr.v4.runtime.{ANTLRInputStream, CommonTokenStream}
 import parser.{ASTVisitor, Error, ErrorListener, PCFParser, ReportingPCFLexer, SyntaxError}
@@ -16,8 +15,7 @@ object Interp:
       then (System.in, None)
       else (new FileInputStream(args(0)), Some(args(0)))
     val verbose = args.length == 0 || args.length > 1 && args(1) == "-v"
-    if (args.contains("-i")) println(s"==> ${interpret()}")
-    else compile()
+    println(s"==> ${interpret()}")
 
     def analyze(): (Term, Type) =
       val input = new ANTLRInputStream(is)
@@ -42,22 +40,3 @@ object Interp:
       val (term, typ) = analyze()
       val value = interp.Interp.interp(term, Map())
       s"$value:$typ"
-
-    def compile(): Unit =
-      val (term, _) = analyze()
-      val aterm = ast.annotate(term, Nil)
-      if (verbose) println(s"annotated AST: $aterm")
-      val code = Gen.gen(aterm)
-      if (filename.isDefined) write(code)
-      else println(code)
-
-    // write code to .wat file associated to .pcf file passed as argument,
-    // returning .wat file relative filename
-    def write(code: String): String =
-      val CFilename = filename.get.replaceFirst("\\.pcf\\z", ".wat")
-      if (verbose) System.out.println("writing .wat code to " + CFilename)
-      val out = new FileWriter(CFilename)
-      out.write(code)
-      out.flush()
-      out.close()
-      CFilename
